ecbbad7175d3838ecfc8bac3dc30878ca2bdbcaa
Amir Zare <amirzare309@yahoo.com>
2024-08-20 02:54:46 -0400

    feat: add user update endpoint.

--- api/v1/users/users.py
+++ api/v1/users/users.py
@@ -3,7 +3,7 @@ from uuid import UUID
 from fastapi import APIRouter, Depends
 
 from app.controllers import UserController
-from app.schemas.request import RegisterUserRequest
+from app.schemas.request import RegisterUserRequest, UpdateUserRequest
 from app.schemas.response import UserResponse
 from core.factory import Factory
 
@@ -18,7 +18,7 @@ async def get_users(user_controller: UserController = Depends(Factory().get_user
     return await user_controller.get_all()
 
 
-@user_router.get("/{user_uuid}")
+@user_router.get("/{id}")
 async def get_user(uuid=UUID, user_controller: UserController = Depends(Factory().get_user_controller)) -> UserResponse:
     """
     Retrieve user by ID.
@@ -41,4 +41,22 @@ async def register_user(
         password=register_user_request.password,
         role=register_user_request.role,
         activated=register_user_request.activated,
+    )
+
+
+@user_router.put("/{id}")
+async def update_user(
+    uuid: UUID,
+    update_user_request: UpdateUserRequest,
+    user_controller: UserController = Depends(Factory().get_user_controller),
+) -> UserResponse:
+    """
+    Update a user.
+    """
+    return await user_controller.update(
+        user_uuid=uuid,
+        email=update_user_request.email,
+        first_name=update_user_request.first_name,
+        last_name=update_user_request.last_name,
+        password=update_user_request.password,
     )
