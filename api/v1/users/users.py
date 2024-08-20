d1598a2efd2546b58ad86c59241b0454637726c7
Amir Zare <amirzare309@yahoo.com>
2024-08-20 01:37:59 -0400

    feat: add get users endpoint.

--- api/v1/users/users.py
+++ api/v1/users/users.py
@@ -10,6 +10,14 @@ from core.factory import Factory
 user_router = APIRouter()
 
 
+@user_router.get("/")
+async def get_users(user_controller: UserController = Depends(Factory().get_user_controller)) -> list[UserResponse]:
+    """
+    Retrieve users.
+    """
+    return await user_controller.get_all()
+
+
 @user_router.get("/{user_uuid}")
 async def get_user(uuid=UUID, user_controller: UserController = Depends(Factory().get_user_controller)) -> UserResponse:
     """
