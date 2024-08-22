d6687e3ace4c5191e38be133828ff09e07ec281f
Amir Zare <amirzare309@yahoo.com>
2024-08-22 10:49:53 -0400

    feat: add a refresh token controller.

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,10 +1,12 @@
+import asyncio
+
 from redis.asyncio import client
 
 from app.models import User
 from app.repositories import UserRepository
 from app.schemas.extras import Token
 from core.controller import BaseController
-from core.exceptions import BadRequestException
+from core.exceptions import BadRequestException, NotFoundException, UnauthorizedException
 from core.security import JWTHandler, PasswordHandler
 
 
@@ -28,6 +30,27 @@ class AuthController(BaseController[User]):
         access_token = JWTHandler.encode(payload={"uuid": str(user.uuid), "role": user.role})
 
         await cache.set(name=refresh_token, value=str(user.uuid), ex=JWTHandler.refresh_token_expire)
+        return Token(
+            access_token=access_token,
+            refresh_token=refresh_token,
+        )
+
+    async def refresh(self, *, old_refresh_token: str, cache: client.Redis) -> Token:
+        uuid, ttl = await asyncio.gather(cache.get(old_refresh_token), cache.ttl(old_refresh_token))
+        if not uuid:
+            raise UnauthorizedException(message="Invalid token.")
+
+        user = await self.user_repository.get_by_uuid(uuid=uuid)
+        if not user:
+            raise NotFoundException(message="User not found.")
+
+        access_token = JWTHandler.encode(payload={"uuid": str(uuid), "role": user.role})
+        refresh_token = JWTHandler.encode_refresh_token(
+            payload={"sub": "refresh_token", "verify": str(uuid), "role": user.role}
+        )
+
+        await asyncio.gather(cache.set(name=refresh_token, value=uuid, ex=ttl), cache.delete(old_refresh_token))
+
         return Token(
             access_token=access_token,
             refresh_token=refresh_token,
