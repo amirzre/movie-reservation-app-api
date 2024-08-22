6aa6d94db8a179e197affc0bc1611316a8646ced
Amir Zare <amirzare309@yahoo.com>
2024-08-22 10:50:13 -0400

    feat: add a refresh token endpoint.

--- api/v1/auth/auth.py
+++ api/v1/auth/auth.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Request, Response, status
 from redis.asyncio import client
 
 from app.controllers import AuthController
 from app.schemas.request import UserLoginRequest
+from core.exceptions import NotFoundException
 from core.factory import Factory
-from core.fastapi.dependencies import get_cache
+from core.fastapi.dependencies import get_cache, get_current_user_with_refresh_token
 from core.security import JWTHandler
 
 auth_router = APIRouter()
@@ -25,6 +26,43 @@ async def login(
         password=login_user_request.password,
         cache=cache,
     )
+
+    response.set_cookie(
+        key="Access-Token",
+        value=tokens.access_token,
+        secure=True,
+        httponly=True,
+        samesite="strict",
+        expires=JWTHandler.token_expiration(tokens.access_token),
+    )
+    response.set_cookie(
+        key="Refresh-Token",
+        value=tokens.refresh_token,
+        secure=True,
+        httponly=True,
+        samesite="strict",
+        expires=JWTHandler.token_expiration(tokens.refresh_token),
+    )
+
+
+@auth_router.post(
+    "/refresh", status_code=status.HTTP_200_OK, dependencies=[Depends(get_current_user_with_refresh_token)]
+)
+async def refresh(
+    request: Request,
+    response: Response,
+    cache: client.Redis = Depends(get_cache),
+    auth_controller: AuthController = Depends(Factory().get_auth_controller),
+) -> None:
+    """
+    Retrieve new access and refresh token.
+    """
+    tokens = await auth_controller.refresh(
+        old_refresh_token=request.cookies.get("Refresh-Token", ""),
+        cache=cache,
+    )
+    if not tokens.access_token:
+        raise NotFoundException(message="Access token not found.")
 
     response.set_cookie(
         key="Access-Token",
