fea0e6cdd0b1c9280955a8f84c621f3dd5ea1f77
Amir Zare <amirzare309@yahoo.com>
2024-08-26 04:28:40 -0400

    feat: add d test structure and conftest for apis.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,9 @@
-from typing import AsyncGenerator
-
 import pytest_asyncio
-from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 import core.db.transactional as transactional
 from app.models import Base
 from core.config import config
-from core.db import get_session
-from core.server import app
 
 engine = create_async_engine(config.POSTGRES_TEST_URL.unicode_string(), pool_pre_ping=True)
 
@@ -19,15 +14,6 @@ async_session_local = async_sessionmaker(
     autocommit=False,
     autoflush=False,
 )
-
-
-async def override_get_session() -> AsyncGenerator:
-    async with async_session_local() as session:
-        yield session
-        await session.commit()
-
-
-app.dependency_overrides[get_session] = override_get_session
 
 
 @pytest_asyncio.fixture(scope="function")
@@ -46,9 +32,3 @@ async def db_session() -> AsyncSession:
         pass
 
     await async_engine.dispose()
-
-
-@pytest_asyncio.fixture(scope="module")
-async def client() -> AsyncClient:
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        yield client
