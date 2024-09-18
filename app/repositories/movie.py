b1dcf86cd0b6b7688f20abdd4938bdc54644ee38
Amir Zare <amirzare309@yahoo.com>
2024-09-18 02:16:01 -0400

    feat: add get by uuid repository.

--- app/repositories/movie.py
+++ app/repositories/movie.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from app.models import Movie
 from core.repository import BaseRepository
 
@@ -7,4 +9,18 @@ class MovieRepository(BaseRepository[Movie]):
     Movie repository provides all the database operations for the Movie model.
     """
 
-    ...
+    async def get_by_uuid(self, uuid: UUID, join_: set[str] | None = None) -> Movie | None:
+        """
+        Get movie by uuid.
+
+        :param uuid: Movie uuid.
+        :param join_: Join relations.
+        :return: Movie.
+        """
+        query = self._query(join_)
+        query = query.filter(Movie.uuid == uuid)
+
+        if join_ is not None:
+            return await self._all_unique(query)
+
+        return await self._one_or_none(query)
