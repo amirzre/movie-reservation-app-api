379fffeaea50363a6830ce335411abbda05dfd74
Amir Zare <amirzare309@yahoo.com>
2024-09-18 02:16:56 -0400

    feat: add get movie controller.

--- app/controllers/movie.py
+++ app/controllers/movie.py
@@ -1,6 +1,9 @@
+from uuid import UUID
+
 from app.models import Movie
 from app.repositories import MovieRepository
 from core.controller import BaseController
+from core.exceptions import NotFoundException
 
 
 class MovieController(BaseController[Movie]):
@@ -12,4 +15,8 @@ class MovieController(BaseController[Movie]):
         super().__init__(model=Movie, repository=movie_repository)
         self.movie_repository = movie_repository
 
-    ...
+    async def get_movie(self, *, movie_uuid: UUID) -> Movie:
+        movie = await self.movie_repository.get_by_uuid(uuid=movie_uuid)
+        if not movie:
+            raise NotFoundException(message="Movie not found.")
+        return movie
