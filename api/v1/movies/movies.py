806f48c4c49b3636750b1b38316eb4c2ab0f6392
Amir Zare <amirzare309@yahoo.com>
2024-09-18 03:51:15 -0400

    feat: add create movie endpoint.

--- api/v1/movies/movies.py
+++ api/v1/movies/movies.py
@@ -1,10 +1,12 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from app.controllers import MovieController
+from app.schemas.request import CreateMovieRequest
 from app.schemas.response import MovieResponse
 from core.factory import Factory
+from core.fastapi.dependencies import ADMINISTRATIVE, RoleChecker
 
 movie_router = APIRouter()
 
@@ -28,3 +30,20 @@ async def get_movie(
     Retrieve movie by ID.
     """
     return await movie_controller.get_movie(movie_uuid=id)
+
+
+@movie_router.post("/", status_code=status.HTTP_201_CREATED, dependencies=[Depends(RoleChecker(ADMINISTRATIVE))])
+async def create(
+    create_movie_request: CreateMovieRequest,
+    movie_controller: MovieController = Depends(Factory().get_movie_controller),
+) -> MovieResponse:
+    """
+    Create new movie.
+    """
+    return await movie_controller.create_movie(
+        title=create_movie_request.title,
+        description=create_movie_request.description,
+        genre=create_movie_request.genre,
+        release_date=create_movie_request.release_date,
+        activated=create_movie_request.activated,
+    )
