6d67b995429af2cd30ea9efad5e7e93ce9fcfa53
Amir Zare <amirzare309@yahoo.com>
2024-09-18 02:17:46 -0400

    feat: add get movies and get movie endpoints.

--- api/v1/movies/movies.py
+++ api/v1/movies/movies.py
@@ -1,3 +1,30 @@
-from fastapi import APIRouter
+from uuid import UUID
+
+from fastapi import APIRouter, Depends
+
+from app.controllers import MovieController
+from app.schemas.response import MovieResponse
+from core.factory import Factory
 
 movie_router = APIRouter()
+
+
+@movie_router.get("/")
+async def get_movies(
+    movie_controller: MovieController = Depends(Factory().get_movie_controller),
+) -> list[MovieResponse]:
+    """
+    Retrieve movies.
+    """
+    return await movie_controller.get_all()
+
+
+@movie_router.get("/{id}")
+async def get_movie(
+    id: UUID,
+    movie_controller: MovieController = Depends(Factory().get_movie_controller),
+) -> MovieResponse:
+    """
+    Retrieve movie by ID.
+    """
+    return await movie_controller.get_movie(movie_uuid=id)
