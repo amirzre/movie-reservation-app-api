888ecec449a874fe2a0ee1a332adbd13a0aa277a
Amir Zare <amirzare309@yahoo.com>
2024-09-23 01:36:00 -0400

    feat: add showtime router.

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -2,9 +2,11 @@ from fastapi import APIRouter
 
 from .auth import auth_routers
 from .movies import movies_router
+from .showtimes import showtimes_router
 from .users import users_router
 
 v1_router = APIRouter()
 v1_router.include_router(auth_routers, prefix="/auth")
 v1_router.include_router(users_router, prefix="/users")
 v1_router.include_router(movies_router, prefix="/movies")
+v1_router.include_router(showtimes_router, prefix="/showtimes")
---
+++ api/v1/showtimes/showtimes.py
@@ -0,0 +1,3 @@
+from fastapi import APIRouter
+
+showtime_router = APIRouter()
