ed071037d4787306e7b907f20e4c2ba55fa4f306
Amir Zare <amirzare309@yahoo.com>
2024-09-23 03:36:17 -0400

    feat: add get showtimes and join movie method.

--- app/repositories/showtime.py
+++ app/repositories/showtime.py
@@ -1,5 +1,8 @@
 from uuid import UUID
 
+from sqlalchemy import Select
+from sqlalchemy.orm import joinedload
+
 from app.models import Showtime
 from core.repository import BaseRepository
 
@@ -8,6 +11,20 @@ class ShowtimeRepository(BaseRepository[Showtime]):
     """
     Showtime repository provides all the database operations for the Showtime model.
     """
+
+    async def get_showtimes(self, join_: set[str] | None = None) -> list[Showtime]:
+        """
+        Retrieve all showtimes with their related movies.
+
+        :param join_: Join relations.
+        :return: Showtimes list.
+        """
+        query = self._query(join_)
+
+        if join_ and "movie" in join_:
+            query = self._join_movie(query)
+
+        return await self._all(query)
 
     async def get_by_uuid(self, uuid: UUID, join_: set[str] | None = None) -> Showtime | None:
         """
@@ -24,3 +41,12 @@ class ShowtimeRepository(BaseRepository[Showtime]):
             return await self._all_unique(query)
 
         return await self._one_or_none(query)
+
+    def _join_movie(self, query: Select) -> Select:
+        """
+        Join the moive relationship.
+
+        :param query: The query to join.
+        :return: The joined query.
+        """
+        return query.options(joinedload(Showtime.movie))
