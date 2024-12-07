d5d32b3b7af7cb958d0a0052151827399210f88b
Amir Zare <amirzare309@yahoo.com>
2024-12-07 13:31:32 +0330

    feat: add get filterd movies endpoint.

--- api/v1/movies/movies.py
+++ api/v1/movies/movies.py
@@ -1,8 +1,11 @@
-from fastapi import APIRouter, Depends, status
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, Query, status
 from pydantic import UUID4
 
 from app.controllers import MovieController
-from app.schemas.request import CreateMovieRequest, UpdateMovieRequest
+from app.schemas.extras import PaginationResponse
+from app.schemas.request import CreateMovieRequest, MovieFilterParams, UpdateMovieRequest
 from app.schemas.response import MovieResponse
 from core.factory import Factory
 from core.fastapi.dependencies import ADMINISTRATIVE, RoleChecker, get_authenticated_user
@@ -12,12 +15,13 @@ movie_router = APIRouter()
 
 @movie_router.get("/", dependencies=[Depends(get_authenticated_user)])
 async def get_movies(
+    filter_params: Annotated[MovieFilterParams, Query()],
     movie_controller: MovieController = Depends(Factory().get_movie_controller),
-) -> list[MovieResponse]:
+) -> PaginationResponse[MovieResponse]:
     """
     Retrieve movies.
     """
-    return await movie_controller.get_all()
+    return await movie_controller.get_movies(filter_params=filter_params)
 
 
 @movie_router.get("/{id}", dependencies=[Depends(get_authenticated_user)])
