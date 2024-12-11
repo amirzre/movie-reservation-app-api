b8175ce6ec90c9a7e3c91a2ce899baedf6cef7b5
Amir Zare <amirzare309@yahoo.com>
2024-12-11 12:55:14 +0330

    feat: add seat factory.

--- core/factory/factory.py
+++ core/factory/factory.py
@@ -2,9 +2,9 @@ from functools import partial
 
 from fastapi import Depends
 
-from app.controllers import AuthController, MovieController, ShowtimeController, UserController
-from app.models import Movie, Showtime, User
-from app.repositories import MovieRepository, ShowtimeRepository, UserRepository
+from app.controllers import AuthController, MovieController, SeatController, ShowtimeController, UserController
+from app.models import Movie, Seat, Showtime, User
+from app.repositories import MovieRepository, SeatRepository, ShowtimeRepository, UserRepository
 from core.db import get_session
 
 
@@ -17,6 +17,7 @@ class Factory:
     user_repository = partial(UserRepository, User)
     movie_repository = partial(MovieRepository, Movie)
     showtime_repository = partial(ShowtimeRepository, Showtime)
+    seat_repository = partial(SeatRepository, Seat)
 
     def get_user_controller(self, db_session=Depends(get_session)):
         return UserController(user_repository=self.user_repository(db_session=db_session))
@@ -29,3 +30,6 @@ class Factory:
 
     def get_showtime_controller(self, db_session=Depends(get_session)):
         return ShowtimeController(showtime_repository=self.showtime_repository(db_session=db_session))
+
+    def get_seat_controller(self, db_session=Depends(get_session)):
+        return SeatController(seat_repository=self.seat_repository(db_session=db_session))
