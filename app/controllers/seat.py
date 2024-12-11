3961c6a7222643fd710b05c486a0b5478edb2c06
Amir Zare <amirzare309@yahoo.com>
2024-12-11 12:50:38 +0330

    feat: add get seats controller.

--- app/controllers/seat.py
+++ app/controllers/seat.py
@@ -1,5 +1,8 @@
 from app.models import Seat
 from app.repositories import SeatRepository
+from app.schemas.extras import PaginationResponse
+from app.schemas.request import SeatFilterParams
+from app.schemas.response import SeatResponse
 from core.controller import BaseController
 
 
@@ -11,3 +14,10 @@ class SeatController(BaseController[Seat]):
     def __init__(self, seat_repository: SeatRepository):
         super().__init__(model=Seat, repository=seat_repository)
         self.seat_repository = seat_repository
+
+    async def get_seats(self, *, filter_params: SeatFilterParams) -> PaginationResponse[SeatResponse]:
+        seats, total = await self.seat_repository.get_filtered_seats(filter_params=filter_params)
+
+        return PaginationResponse[SeatResponse](
+            limit=filter_params.limit, offset=filter_params.offset, total=total, items=seats
+        )
